0cae3993a2eaf1ee9f5088d764def819ea658c09
andkymon <andkymon@gmail.com>
2025-02-09 15:58:26 +0800

    Test knightMoves()

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { knightMoves } from "./knight-moves.js";
 
+knightMoves([0,0], [0,0]);
 knightMoves([0,0], [7,7]);
-knightMoves([0,0], [6,7]);
+knightMoves([0,0], [6,6]);
 knightMoves([0,0], [8,8]);
